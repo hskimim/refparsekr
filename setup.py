df2201e97c5e762dd5b27eb653a8c1190379c715
hskimim <hskimim8855@gmail.com>
2018-12-27 15:40:23 +0900

    refresh

--- setup.py
+++ setup.py
@@ -1,41 +1,22 @@
-# -*- coding: utf-8 -*-
-
-import sys
-
 from setuptools import find_packages, setup
 from setuptools.command.test import test
+
 
 setup_requires = [
     ]
 
-install_requires = [
-    'pandas==0.23.4',
-    'numpy==1.15.1',
-    ]
-
-dependency_links = [
-    ]
+requirements = ['setuptools', 'numpy', 'pandas']
 
 
 setup(
-    name='parsing_the_references',
+    name='refparsekr',
     version='0.1',
-    license='GNU GENERAL PUBLIC LICENSE',
+    description='references parsing in korean',
     author='hskimim',
     author_email='hskimim8855@gmail.com',
-    url='https://github.com/hskimim/refparsekr',
-    description='Parse the references line by line',
-    packages=['ML'],
     include_package_data=True,
-    install_requires=install_requires,
+    install_requires=requirements,    
     setup_requires=setup_requires,
-    dependency_links=dependency_links,
-
-entry_points={
-        'console_scripts': [
-            ],
-        "egg_info.writers": [
-                "foo_bar.txt = setuptools.command.egg_info:write_arg",
-            ],
-        },
-    )
+    classifiers=[],
+    
+)
