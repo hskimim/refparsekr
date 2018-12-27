d4f23c9e3f7f444a739f2fcd98536464a8d6d652
hskimim <hskimim8855@gmail.com>
2018-12-27 16:02:08 +0900

    refresh

--- setup.py
+++ setup.py
@@ -1,22 +1,35 @@
-from setuptools import find_packages, setup
-from setuptools.command.test import test
-
+from setuptools import setup, find_packages
 
 setup_requires = [
     ]
 
-requirements = ['setuptools', 'numpy', 'pandas']
+install_requires = [
+    "pdfminer.six==20181108"
+    "pandas==0.23.4"
+    "numpy==1.15.1"
 
+    ]
+
+dependency_links = [
+    ]
 
 setup(
     name='refparsekr',
     version='0.1',
-    description='references parsing in korean',
+    description='refernecs parsing in Korean',
     author='hskimim',
     author_email='hskimim8855@gmail.com',
+    packages=["CA"],
     include_package_data=True,
-    install_requires=requirements,    
+    install_requires=install_requires,
     setup_requires=setup_requires,
-    classifiers=[],
-    
-)
+    dependency_links=dependency_links,
+    # scripts=['manage.py'],
+    entry_points={
+        'console_scripts': [
+            ],
+        "egg_info.writers": [
+                "foo_bar.txt = setuptools.command.egg_info:write_arg",
+            ],
+        },
+    )
